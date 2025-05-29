59ade810278d3cb5370ca0a58d1961a77696d526
Kevin Estrada <estrada.kevin.19@hotmail.com>
2025-05-29 11:50:57 -0600

    Menu desplegable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.aplicacioncrud"
     compileSdk = 35
+    buildFeatures{
+        viewBinding=true;
+    }
 
     defaultConfig {
         applicationId = "com.example.aplicacioncrud"
@@ -42,6 +45,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/aplicacioncrud/MainActivity.kt
+++ app/src/main/java/com/example/aplicacioncrud/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.aplicacioncrud
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.aplicacioncrud.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var drawerLayout=findViewById<DrawerLayout>(R.id.drawellayout)
+        var tollbar=findViewById<MaterialToolbar>(R.id.tollbar)
+        val navigationView = findViewById<NavigationView>(R.id.navigation)
+
+        tollbar.setOnClickListener(View.OnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        })
+        replaceFragment(Op_Consultar())
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.consultar -> replaceFragment(Op_Consultar())
+                R.id.Agregar -> replaceFragment(Op_Insertar())
+                R.id.Editar -> replaceFragment(Op_editar())
+                R.id.Eliminar -> replaceFragment(Op_Eliminar())
+                R.id.Logout -> finish()
+
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
         }
     }
-}
